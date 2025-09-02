59f13d734ab5da8f776a7dd8a68b4d8177c7c228
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-09-02 11:18:12 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,9 @@
+const perguntas = [
+    {
+        enunciado: "pergunta 1", //a pergunta
+        alternativas; [
+        {
+            texto: "nike", //primeira alternativa
+            afirmacao: [
+                "vocẽ tem bom gosto", //
 
