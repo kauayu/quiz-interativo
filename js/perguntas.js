250f254aeb41774af26ffc40f72a4ccd77913622
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-09-15 10:26:21 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,9 +1,95 @@
-const perguntas = [
+export const perguntas = [
     {
         enunciado: "pergunta 1", //a pergunta
         alternativas; [
         {
             texto: "nike", //primeira alternativa
             afirmacao: [
-                "vocẽ tem bom gosto", //
+                "vocẽ tem bom gosto", //comentario para a resposta 
+                "afirmação 2"        //segundo comentario
+            ],
+        },
+        {
+              texto: "adidas",
+              afirmacao: [
+                "você é vaiado",
+                "você não sabe escolher"
+              ],
+           }
+        ]
+    },
+        
 
+
+                proxima: 1,
+
+
+
+
+
+
+
+                proxima: 2;
+
+
+
+
+
+
+
+
+
+
+
+
+                proxima: 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                proxima: 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                proxima: 4;
+
+
+
+
+
+
+
+                proxima: 4;
