517af73d3f5d399c96634acbafdd840f99052bba
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-09-16 12:00:25 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import {aleatorio} from './aleatorio.js';
-import {perguntas} from './perguntas.js;
+import {aleatorio, nome} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
 
 const caixaprincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
