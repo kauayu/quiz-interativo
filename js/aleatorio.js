9e047795c714755549af73cdaf48ee10fd36eed8
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-09-16 11:58:19 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
- const nones = [ "pedro", "juca", "pietra", "felca", "pedro", "joão",]
+ const nones = [ "pedro", "juca", "pietra", "felca", "pedro", "joão",];
+
+ export function aleatorio )(lista){
+    const posicao = Math. floor(Math.random()* lista.length);
+    return lista[posicao];
+ }
+
+ export const nome = aleatorio(nomes)
